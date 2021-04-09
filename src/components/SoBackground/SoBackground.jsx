9a2623534306b3ef3408b4e8f5a3ff5353c30500
unknown <rzomber123@gmail.com>
2021-04-08 21:29:35 -0300

    video local

--- src/components/SoBackground/SoBackground.jsx
+++ src/components/SoBackground/SoBackground.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './SoBackground.css';
+import video from './video.mp4';
 
 const SoBackground = () => {
     return (<React.Fragment>
         <video autoPlay loop muted id="video">
-            <source src={`${process.env.REACT_APP_CLIENT_IP}:${process.env.REACT_APP_CLIENT_PORT}/video.mp4`} type="video/mp4" />
+            <source src={video} type="video/mp4" />
         </video>
         <div className="video-opacity"></div>
     </React.Fragment>)
